fd3e77011b84a37db55b797049c9a232f317a379
Zunden <33324339+znuden@users.noreply.github.com>
2019-07-10 13:29:19 +0200

    Enemy

---
+++ Sommar19Spel/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] private float health;
+    [SerializeField] private float speed;
+    [SerializeField] private Transform player;
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3.MoveTowards(transform.position, player.transform.position, speed);
+    }
+}
