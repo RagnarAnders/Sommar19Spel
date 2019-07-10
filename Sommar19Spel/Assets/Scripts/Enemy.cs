7ad290e51f118080b57c024a2398de7c8df04051
Zunden <33324339+znuden@users.noreply.github.com>
2019-07-10 14:02:53 +0200

    Update Enemy.cs

--- Sommar19Spel/Assets/Scripts/Enemy.cs
+++ Sommar19Spel/Assets/Scripts/Enemy.cs
@@ -17,7 +17,7 @@ public class Enemy : MonoBehaviour
     {
         float maxDistanceDelta = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, Player.PlayerReference.transform.position, maxDistanceDelta);
+        //transform.LookAt(Player.PlayerReference.transform.position);
     }
 
-   
 }
