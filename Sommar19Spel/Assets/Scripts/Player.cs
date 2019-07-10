07f146177089ed5e97f7dd769ed02cfa45d1802c
RagnarAnders <37663886+RagnarAnders@users.noreply.github.com>
2019-07-10 14:02:41 +0200

    player rotates

--- Sommar19Spel/Assets/Scripts/Player.cs
+++ Sommar19Spel/Assets/Scripts/Player.cs
@@ -28,5 +28,18 @@ public class Player : MonoBehaviour
         Vector2 movement = new Vector2(horizontal, vertical);
 
         transform.position += (Vector3)movement * speed * Time.deltaTime;
+
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 rotateTo = (mouseScreenPosition - (Vector2)transform.position).normalized;
+
+        transform.up = rotateTo;
+
+        //float AngleRad = Mathf.Atan2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x,
+        //    Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y);
+
+        //float AngleDeg = (180 / Mathf.PI) * AngleRad;
+
+        //transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
     }
 }
