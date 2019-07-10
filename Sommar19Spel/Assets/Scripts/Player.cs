43420ae0897a59a5997269a5b59cccd28a90a8ef
RagnarAnders <37663886+RagnarAnders@users.noreply.github.com>
2019-07-10 13:32:46 +0200

    playerRef

---
+++ Sommar19Spel/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    public Player PlayerReference { get; private set; }
+
+    private void Awake()
+    {
+        if(PlayerReference == null)
+        {
+            PlayerReference = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(horizontal, vertical);
+
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+    }
+}
