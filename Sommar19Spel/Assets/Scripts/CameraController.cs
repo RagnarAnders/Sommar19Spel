40f7ad5f27f69fd8639722acbb9dcef16dc98d3b
Zunden <33324339+znuden@users.noreply.github.com>
2019-07-11 10:30:13 +0200

    sdads

---
+++ Sommar19Spel/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{   
+    [SerializeField]
+    private Vector3 offset;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Player.PlayerReference.transform;
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(player != null)
+        {
+            transform.position = player.position + offset;
+        }
+    }
+}
---
+++ Sommar19Spel/Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float spawnRate;
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer <= 0)
+        {
+            Spawn();
+            timer = spawnRate;
+        }
+        timer -= Time.deltaTime;
+    }
+
+    private void Spawn()
+    {
+        Instantiate(enemyPrefab, Vector2.zero, Quaternion.identity);
+    }
+}
