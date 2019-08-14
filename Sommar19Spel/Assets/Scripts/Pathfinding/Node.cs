1eaa05cd7e4e12091ee6a2c2a878b9e0dba73782
RagnarAnders <37663886+RagnarAnders@users.noreply.github.com>
2019-08-14 10:24:24 +0200

    Pathfindning
    Börjat på a* för fienden

---
+++ Sommar19Spel/Assets/Scripts/Pathfinding/Node.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeValue
+{
+    CLEAR = 0,
+    BLOCKED = 1
+}
+
+public class Node
+{
+    Vector2 position;
+    NodeValue walkable;
+
+    public Node(Vector2 position, NodeValue walkable)
+    {
+        this.position = position;
+        this.walkable = walkable;
+    }
+}
