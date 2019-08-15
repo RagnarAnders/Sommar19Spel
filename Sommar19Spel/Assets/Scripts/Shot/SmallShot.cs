76daecbf5a500ffd45b5585b395ee00b2f715caf
Zunden <33324339+znuden@users.noreply.github.com>
2019-08-15 13:05:44 +0200

    asdsaf

--- Sommar19Spel/Assets/Scripts/Shot/SmallShot.cs
+++ Sommar19Spel/Assets/Scripts/Shot/SmallShot.cs
@@ -10,6 +10,7 @@ public class SmallShot : MonoBehaviour
 
     public LayerMask collisionMask;
     public float detection = 0.01f;
+    int counter = 0;
 
     void Start()
     {
@@ -17,49 +18,44 @@ public class SmallShot : MonoBehaviour
         direction = Player.PlayerReference.transform.up;
     }
 
-    private void Update()
+    void Update()
     {
-        //Velocity = direction * Time.deltaTime * moveSpeed;
-        //transform.position += (Vector3)Velocity;
-
+  
         transform.position += transform.up * speed * Time.deltaTime;
 
         Ray2D ray = new Ray2D(transform.position, transform.up);
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Time.deltaTime * speed + detection, collisionMask);
         if (hit.collider != null)
-        {
-            Debug.Log("Hit");
-            //Debug.DrawLine(ray.origin, hit.point, Color.red);
+        { 
             Vector2 v = Vector2.Reflect(ray.direction, hit.normal);
-            Debug.Log("New vector: " + v);
             float rot = Mathf.Atan2(v.x, v.y) * Mathf.Rad2Deg;
-            Debug.DrawRay(hit.point, v, Color.red);
-            Debug.Log("Rotation: " + rot);
             transform.up = v;
-            //transform.eulerAngles = new Vector3(0, 0, rot);
+            counter++;
+            speed += 0.5f;
         }
     }
 
-    //protected void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    //Debug.Log("Collision");
-    //    if (collision.otherCollider.CompareTag("Enemy") || collision.Equals(Player.PlayerReference.GetComponent<Collider2D>()))
-    //    {
-    //        collision.gameObject.GetComponent<HealthComponent>().TakeDamage(50);
-    //        ShakeCamera.ShakeCameraRef.Shake(0.01f, 0.2f);
+    protected void OnTriggerEnter2D(Collider2D collider)
+    {
+        //Debug.Log("Collision");
+        if (collider.CompareTag("Enemy"))
+        {
+            collider.gameObject.GetComponent<HealthComponent>().TakeDamage(50);
+            ShakeCamera.ShakeCameraRef.Shake(0.01f, 0.2f);
 
-    //        Destroy(gameObject);
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Velocity Before: " + Velocity);
+            Destroy(gameObject);
+        }
+        else if (collider.Equals(Player.PlayerReference.GetComponent<Collider2D>()))
+        {
+            if(counter > 0)
+            {
+                Player.PlayerReference.Damaged();
+                Destroy(gameObject);
+            }
+         
 
-    //        direction = Vector2.Reflect(Velocity, collision.contacts[0].normal);
-    //        Velocity = Vector2.zero;
-    //        Debug.Log("Velocity After: " + Velocity);
-
-    //    }
+        }
 
 
-    //}
+    }
 }
