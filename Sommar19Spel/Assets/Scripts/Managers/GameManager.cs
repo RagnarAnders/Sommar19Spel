b17ab78edb4214a88ba2b58ee28b39c3406c9a79
Simon <simon.sundstrom95@gmail.com>
2019-08-21 19:55:53 +0200

    Main menu

--- Sommar19Spel/Assets/Scripts/Managers/GameManager.cs
+++ Sommar19Spel/Assets/Scripts/Managers/GameManager.cs
@@ -5,8 +5,68 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public void Play()
+
+    private bool _isGameOver;
+    public bool IsGameOver { get { return _isGameOver; } }
+
+    [SerializeField]
+    private string nextLevelName;
+
+    [SerializeField]
+    private int nextLevelIndex;
+
+    // end the level
+    public void EndLevel()
     {
-        SceneManager.LoadScene(1);
+        
+
+        // check if we have set IsGameOver to true, only run this logic once
+        if (!_isGameOver)
+        {
+            _isGameOver = true;
+
+            LoadNextLevel();
+
+        }
     }
+
+    public void LoadLevel(string levelName)
+    {
+        if (Application.CanStreamedLevelBeLoaded(levelName))
+        {
+            SceneManager.LoadScene(levelName);
+        }
+        else
+        {
+            Debug.LogWarning("GAMEMANAGER LoadLevel Error: invalid scene specified!");
+        }
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        if (levelIndex >= 0 && levelIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(levelIndex);
+        }
+        else
+        {
+            Debug.LogWarning("GAMEMANAGER LoadLevel Error: invalid scene specified!");
+        }
+    }
+
+    public void ReloadLevel()
+    {
+        LoadLevel(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadNextLevel()
+    {
+
+        int nextSceneIndex = (SceneManager.GetActiveScene().buildIndex + 1) %
+            SceneManager.sceneCountInBuildSettings;
+
+        LoadLevel(nextSceneIndex);
+
+    }
+
 }
