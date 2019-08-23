2047a88b69d683b0f7ae57659727748e9a83b8cb
Zunden <33324339+znuden@users.noreply.github.com>
2019-08-23 09:50:45 +0200

    Main Menu

--- Sommar19Spel/Assets/LevelManagedment/Scripts/Menu.cs
+++ Sommar19Spel/Assets/LevelManagedment/Scripts/Menu.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LevelManagement
-{
-    [RequireComponent(typeof(Canvas))]
-    public class Menu : MonoBehaviour
-    {
-        public void OnBackPressed()
-        {
 
+
+[RequireComponent(typeof(Canvas))]
+public class Menu : MonoBehaviour
+{
+    public void OnPlayPressed()
+    {
+        GameManager gameManager = Object.FindObjectOfType<GameManager>();
+        if(gameManager != null)
+        {
+            gameManager.LoadNextLevel();
         }
-    } 
+    }
+
+    public void OnSettingsPressed()
+    {
+
+    }
+
+    public void OnCreditsPressed()
+    {
+
+    }
+
+    public void OnBackPressed()
+    {
+
+    }
 }
+
--- Sommar19Spel/Assets/LevelManagedment/Scripts/MenuManager.cs
+++ Sommar19Spel/Assets/LevelManagedment/Scripts/MenuManager.cs
@@ -2,47 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LevelManagement
+
+
+public class MenuManager : MonoBehaviour
 {
-    public class MenuManager : MonoBehaviour
+    public Menu mainMenuPrefab;
+    public Menu settingsMenuPrefab;
+    public Menu creditsScreenPrefab;
+
+    [SerializeField]
+    private Transform menuParent;
+
+    private Stack<Menu> menuStack = new Stack<Menu>();
+
+    private void Awake()
     {
-        public Menu mainMenuPrefab;
-        public Menu settingsMenuPrefab;
-        public Menu creditsScreenPrefab;
+        InitializeMenus();
+    }
 
-        [SerializeField]
-        private Transform menuParent;
-
-        private void Awake()
+    private void InitializeMenus()
+    {
+        if (menuParent == null)
         {
-            InitializeMenus();
+            GameObject menuParentObject = new GameObject("Menus");
+            menuParent = menuParentObject.transform;
         }
 
-        private void InitializeMenus()
+        Menu[] menuPrefabs = { mainMenuPrefab, settingsMenuPrefab, creditsScreenPrefab };
+
+        foreach (Menu prefab in menuPrefabs)
         {
-            if(menuParent == null)
+            if (prefab != null)
             {
-                GameObject menuParentObject = new GameObject("Menus");
-                menuParent = menuParentObject.transform;
-            }
-
-            Menu[] menuPrefabs = { mainMenuPrefab, settingsMenuPrefab, creditsScreenPrefab };
-
-            foreach(Menu prefab in menuPrefabs)
-            {
-                if( prefab != null)
+                Menu menuInstance = Instantiate(prefab, menuParent);
+                if (prefab != mainMenuPrefab)
                 {
-                    Menu menuInstance = Instantiate(prefab, menuParent);
-                    if(prefab != mainMenuPrefab)
-                    {
-                        menuInstance.gameObject.SetActive(false);
-                    }
-                    else
-                    {
-                        //open main menu
-                    }
+                    menuInstance.gameObject.SetActive(false);
+                }
+                else
+                {
+                    OpenMenu(menuInstance);
                 }
             }
         }
-    } 
+    }
+
+    public void OpenMenu(Menu menuInstance)
+    {
+        if(menuInstance == null)
+        {
+            Debug.LogWarning("MENUMANAGER OpenMenu ERROR: invalid menu");
+            return;
+        }
+
+        if(menuStack.Count > 0)
+        {
+            foreach(Menu menu in menuStack)
+            {
+                menu.gameObject.SetActive(false);
+            }
+        }
+
+        menuInstance.gameObject.SetActive(true);
+        menuStack.Push(menuInstance);
+    }
+
+    public void CloseMenu()
+    {
+        if(menuStack.Count == 0)
+        {
+            Debug.LogWarning("MENUMANAGER CloseMenu ERROR: No menus in stack!");
+            return;
+        }
+
+        Menu topMenu = menuStack.Pop();
+        topMenu.gameObject.SetActive(false);
+
+        if(menuStack.Count > 0)
+        {
+            Menu nextMenu = menuStack.Peek();
+            nextMenu.gameObject.SetActive(true);
+        }
+    }
 }
+
